cf6487d80b2f2fd317531d284ab11915cb641f4c
Sebastian Gonzalez <seba.gonza887@gmail.com>
2021-08-05 11:38:30 -0300

    subo cambios

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var novedadesModel = require('../models/novedadesModel');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-   var novedades = await novedadesModel.getNovedades();
-  res.render('index', {
+  var novedades = await novedadesModel.getNovedades();
+    res.render('index', {
     novedades
   });
 });
